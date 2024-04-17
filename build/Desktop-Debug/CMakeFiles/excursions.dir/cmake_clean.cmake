b41adae3832f64a2af230fae900d693cdfa17b64
Alejandro <vaquerolopez.alejandro@gmail.com>
2024-04-17 12:27:52 +0200

    first commit

---
+++ build/Desktop-Debug/CMakeFiles/excursions.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/excursions.dir/main.cpp.o"
+  "CMakeFiles/excursions.dir/main.cpp.o.d"
+  "excursions"
+  "excursions.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/excursions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
